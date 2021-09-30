40f1d70e5974a348b4058fa83d8c28069907c81d
Lucas Santana Duarte <45908363+lucasduartedev@users.noreply.github.com>
2021-09-30 19:36:37 -0300

    Banco de dados MySQL. Implementação e testes

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,4 +1,14 @@
 module.exports = (app) => {
+
+    const mysql = require('mysql2')
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'node'
+    })
+
     app.get('/', (req, res) => {
         res.render('index')
     })
