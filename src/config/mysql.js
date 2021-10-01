64f92da77680287b189df12c100ecbd8153ba51d
Lucas Santana Duarte <45908363+lucasduartedev@users.noreply.github.com>
2021-10-01 17:48:37 -0300

    Aplicando views dinamicas com EJS

--- src/config/mysql.js
+++
@@ -1,3 +0,0 @@
-const mysql = require('mysql2')
-
-module.exports = mysql
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,18 +1,20 @@
+const mysql_conn = require("../config/mysql_conn")
+
 module.exports = (app) => {
 
-    const mysql = require('mysql2')
-
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'node'
-    })
+    const conn = mysql_conn()
 
     app.get('/', (req, res) => {
         res.render('index')
     })
     app.post('/', (req, res) => {
         res.send('POST Request')
+    })
+
+    // Tora para teste do banco de dados
+    app.get('/test', (req, res) => {
+        conn.query('SELECT * FROM table_test', (error, result) => {
+            res.render('test', {dados: result})
+        })
     })
 }
